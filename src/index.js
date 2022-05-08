fff10051188d3553bcd213ad996f812d71c19afd
Tanguy Le Loch <lelochtanguy@gmail.com>
2022-05-08 18:51:35 +0200

    clean masonry

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home/Home';
-import GridTest from './pages/GridTest';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserGraduate, faCode, faVialCircleCheck, faBook, faBriefcase } from '@fortawesome/free-solid-svg-icons';
@@ -13,6 +12,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home nbColumns={2} />
-    <GridTest />
   </React.StrictMode>
 );
