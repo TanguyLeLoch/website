81906fc9c353216d6864e4c1dc68f14b83002c7c
Tanguy Le Loch <lelochtanguy@gmail.com>
2022-05-14 08:58:52 +0200

    remove pgsql

--- src/components/Section/Rubrik/Skills/Skills.jsx
+++ src/components/Section/Rubrik/Skills/Skills.jsx
@@ -19,70 +19,33 @@ function Skills() {
       <StyledCategory>
         <h4>Backend</h4>
         <BundleLogo>
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
-            alt="Java"
-          />
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg"
-            alt="Spring"
-          />
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg"
-            alt="Postgresql"
-          />
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg"
-            alt="Node"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" alt="Java" />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg" alt="Spring" />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" alt="Node" />
 
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg"
-            alt="Mongodb"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg" alt="Mongodb" />
         </BundleLogo>
       </StyledCategory>
       <StyledCategory>
         <h4>Frontend</h4>
         <BundleLogo>
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-            alt="React"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" />
 
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-            alt="Javascript"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="Javascript" />
 
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg"
-            alt="Css"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" alt="Css" />
 
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg"
-            alt="Html"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" alt="Html" />
         </BundleLogo>
       </StyledCategory>
       <StyledCategory>
         <h4>Other tools</h4>
         <BundleLogo>
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg"
-            alt="Vscodes"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg" alt="Vscodes" />
 
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
-            alt="Git"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg" alt="Git" />
 
-          <LogoImg
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg"
-            alt="Jenkins"
-          />
+          <LogoImg src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg" alt="Jenkins" />
         </BundleLogo>
       </StyledCategory>
     </Rubrik>
