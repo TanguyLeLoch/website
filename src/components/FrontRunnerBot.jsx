2402ea9007e3c7d60d67b07700b52b5092a65c7a
Tanguy Le Loch <lelochtanguy@gmail.com>
2022-05-15 20:21:20 +0200

    init front run bot

--- src/components/FrontRunnerBot.jsx
+++ src/components/FrontRunnerBot.jsx
@@ -1,15 +1,32 @@
 import styled from 'styled-components';
-// import colors from '../utils/style/colors';
 import { SectionSubtitle } from '../components/Section/SectionTitle';
-// const { SectionTitle, SectionSubtitle } from '../components/Section/SectionTitle';
+
+import Icon from '../utils/Icon';
 const Container = styled.div`
   height: 30px;
 `;
-// const SectionSubtitle = title.SectionSubtitle;
 function FrontRunnerBot() {
   return (
     <Container>
       <SectionSubtitle>Front runner trading bot</SectionSubtitle>
+      <p>
+        The principle of this bot is to take profit about people who make order with a considerably big slippage. Basically the bot:
+        <ul>
+          <li>
+            <Icon icon="fa-arrow-right" />
+            detect this kind of transaction in the pending transaction list
+          </li>
+          <li>
+            <Icon icon="fa-arrow-right" />
+            make a buy order which should pass just before the target transaction
+          </li>
+          <li>
+            <Icon icon="fa-arrow-right" />
+            make a sell order of the ammount buyed which should pass just after the target transacti
+          </li>
+        </ul>
+        the detail of how to make this is available <a href="https://google.com">here</a>
+      </p>
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import Home from './pages/Home/Home';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserGraduate, faCode, faVialCircleCheck, faBook, faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faUserGraduate, faCode, faVialCircleCheck, faBook, faBriefcase, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faUserGraduate, faCode, faVialCircleCheck, faBook, faBriefcase);
+library.add(faUserGraduate, faCode, faVialCircleCheck, faBook, faBriefcase, faArrowRight);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Home />);
