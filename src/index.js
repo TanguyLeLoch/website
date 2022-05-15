82531722cb2744d98d24d4241bc2169bdd2d8f66
Tanguy Le Loch <lelochtanguy@gmail.com>
2022-05-16 00:14:59 +0200

    add navbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home/Home';
+import Layout from './pages/Layout';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserGraduate, faCode, faVialCircleCheck, faBook, faBriefcase, faArrowRight } from '@fortawesome/free-solid-svg-icons';
@@ -9,4 +9,8 @@ import { faUserGraduate, faCode, faVialCircleCheck, faBook, faBriefcase, faArrow
 library.add(faUserGraduate, faCode, faVialCircleCheck, faBook, faBriefcase, faArrowRight);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home />);
+root.render(
+  <>
+    <Layout />
+  </>
+);
---
+++ src/pages/Layout/index.jsx
@@ -0,0 +1,12 @@
+import MyNavBar from './NavBar';
+import Cv from '../CV';
+
+function Layout() {
+  return (
+    <>
+      <MyNavBar />
+      <Cv />
+    </>
+  );
+}
+export default Layout;
